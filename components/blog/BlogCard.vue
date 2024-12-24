8dfc35487b20ea3dec24092ff6bbd79b1f567744
Gleb Bigma <glebbigma@gmail.com>
2024-12-24 17:27:28 +0200

    Fixed BlogCard with mock data.

--- components/blog/BlogCard.vue
+++ components/blog/BlogCard.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="w-72 mx-auto border rounded-lg shadow-lg overflow-hidden bg-white">
     <div class="h-48 bg-gray-200">
-      <img :src="imageUrl" alt="Blog Image" class="w-full h-full object-cover" />
+      <img :src="imageSrc" alt="Blog Image" class="w-full h-full object-cover" />
     </div>
     <div class="p-4">
       <h2 class="text-xl font-semibold text-gray-800 truncate">{{ title }}</h2>
       <div class="flex justify-between items-center text-gray-600 text-sm mt-2">
         <span>{{ category }}</span>
-        <span>{{ date }}</span>
+        <span>{{ formattedDate }}</span>
       </div>
       <NuxtLink
           :to="`/blog/${id}`"
@@ -20,15 +20,20 @@
 </template>
 
 <script lang="ts" setup>
-import { defineProps } from 'vue';
+import { defineProps, computed } from 'vue';
+import { format } from 'date-fns';
 
 interface BlogCardProps {
   id: string;
   title: string;
   category: string;
-  date: string;
-  imageUrl: string;
+  date: number;
+  imageSrc: string;
 }
 
 const props = defineProps<BlogCardProps>();
+
+const formattedDate = computed(() => {
+  return format(new Date(props.date), 'yy/MM/dd');
+});
 </script>
